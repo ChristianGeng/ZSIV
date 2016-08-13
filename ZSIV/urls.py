9cb3367a5cc65a3c35e7e2131ffdfc32ef83237d
ChristianGeng <christian.c.geng@gmail.com>
2016-08-13 12:02:13 +0200

    ZSIV urls aufgeraeumt

--- ZSIV/urls.py
+++ ZSIV/urls.py
@@ -11,33 +11,37 @@ from ZSIV.views  import JournalCreateView
 from ZSIV.models import Mitarbeiter
 app_name = 'ZSIV'
 
-
-
-
-
-
 urlpatterns = [
-    # ex: /ZSIV/
-    #url(r'^$', DetailView.as_view(),name='home'), # http://127.0.0.1:8000/ZSIV/
-    
-    
-    
-    
-    # Main Page
-    url(r'^$', views.index, name='index'),
 
-    #url(r'^ttt$', MAJViewIndex.as_view(), name='MAview_index'),
-    url(r'^ttt/$', views.JournalCreateView.as_view(), name='add_journal_and_summaries'),
+    # (1) Main Page
+    # (2) Manage Subscriptions
+    # (3) Manage Summaries
+    # (4) Queue and send
+    # (5) Versuche 
     
-    url(r'^queue$',
-        Queuelistview.as_view(
-                         model = Mitarbeiter,
-                         template_name='ZSIV/queue_list.html',
-                         ),
-        name = 'queue'
-        ),
     
-    # Summariey Views
+    # (1) Main Page, static so far
+    url(r'^$', views.index, name='index'),
+    
+
+    # (2) Manage Subscriptions / jeweils ein Listview und ein View, der die Subscriptions managt
+    url(r'^Mitarbeiter.html$', views.indexViewMA.as_view(), name='indexMA'), #http://localhost:8000/ZSIV/Mitarbeiter.html
+    url(r'^MitarbeiterSubscribe/(?P<mitarbeiter_id>[0-9]+)/$', views.MA_Subscribe_Journals, name='MitarbeiterSubscribe'),
+    url(r'^Journals.html$', views.indexViewJournals.as_view(), name='indexJournal'),
+    url(r'^JournalSubscribe/(?P<journal_id>[0-9]+)/$', views.Journal_Subscribe_MAs, name='JournalSubscribe'), #http://localhost:8000/ZSIV/JournalSubscribe/
+
+    
+    
+    
+    # (3) Manage Summaries 
+    
+    # (3a) add, update, delete(nicht implementiert!)) + ein MultiDelete
+    url(r'^Summaries/add/$', SummariesCreateView.as_view(), name='Summaries-add'), #http://localhost:8000/ZSIV/Summaries/add/
+    url(r'^Summaries/update/(?P<pk>[0-9]+)/$', SummariesUpdateView.as_view(), name='Summaries-update'), 
+    #url(r'^Summaries/delete/(?P<pk>[0-9]+)/$', SummariesDeleteView.as_view(), name='Summaries-delete'), # http://localhost:8000/ZSIV/Summaries/delete/93/
+    url(r'^Summaries/delete/$', TestFormstSetView.as_view(), name='Summaries-delete-multi'),
+    
+    # (3b) Summary List Views - alle und nicht versandt
     url(r'^Summaries-all$',
         ListView.as_view(
             model=Summaries,
@@ -47,9 +51,6 @@ urlpatterns = [
             template_name='ZSIV/summaries_list.html', # not required, is the default  
             ),
         name='summaries-index'),
-               
-    
-        # Summariey Views
     url(r'^Summaries-unsent$',
         ListView.as_view(
             model=Summaries, 
@@ -59,22 +60,24 @@ urlpatterns = [
             template_name='ZSIV/summaries_list.html', # not required, is the default  
             ),
         name='summaries-unsent'),
-               
-               
+
     
-    url(r'^Summaries/delete/$', TestFormstSetView.as_view(), name='Summaries-delete-multi'),
-    # Summaries Trias - Add and Update work 
-    url(r'^Summaries/add/$', SummariesCreateView.as_view(), name='Summaries-add'), #http://localhost:8000/ZSIV/Summaries/add/
-    url(r'^Summaries/update/(?P<pk>[0-9]+)/$', SummariesUpdateView.as_view(), name='Summaries-update'),
-    url(r'^Summaries/delete/(?P<pk>[0-9]+)/$', SummariesDeleteView.as_view(), name='Summaries-delete'), # http://localhost:8000/ZSIV/Summaries/delete/93/
     
-    #url(r'^Summaries/(?P<pk>[0-9]+)/delete/$', SummariesDeleteView.as_view(), name='Summaries-delete'),
-    #url(r'^Summaries/(?P<summary_id>[0-9]+)/detail/$',SummariesDetailView.as_view(), name="Summaries-detail"),
     
-    # Subscriptions 
-    url(r'^Mitarbeiter.html$', views.indexViewMA.as_view(), name='indexMA'), #http://localhost:8000/ZSIV/Mitarbeiter.html
-    url(r'^MitarbeiterSubscribe/(?P<mitarbeiter_id>[0-9]+)/$', views.MA_Subscribe_Journals, name='MitarbeiterSubscribe'),
-    url(r'^Journals.html$', views.indexViewJournals.as_view(), name='indexJournal'),
-    url(r'^JournalSubscribe/(?P<journal_id>[0-9]+)/$', views.Journal_Subscribe_MAs, name='JournalSubscribe'), #http://localhost:8000/ZSIV/JournalSubscribe/
+    # (4) Queue and send
+    url(r'^queue$',
+        Queuelistview.as_view(
+                         model = Mitarbeiter,
+                         template_name='ZSIV/queue_list.html',
+                         ),
+        name = 'queue'
+        ),
+    
+    
+    
+    # (5) Versuche     
+    # Ersetzen der Grunddaten (der admin-site)
+    # Versuch, mehrere Journals hinzufuegen - defunct 
+    url(r'^ttt/$', views.JournalCreateView.as_view(), name='add_journal_and_summaries'),
     
 ]
