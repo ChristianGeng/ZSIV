dfb23edbe1c7dbc4c49c465d2a4a6e3bcb925c96
ChristianGeng <christian.c.geng@gmail.com>
2016-11-01 16:41:20 +0100

    admin site update

--- ZSIV/admin.py
+++ ZSIV/admin.py
@@ -17,13 +17,13 @@ class majournalInline(admin.StackedInline):
     extra=1
     
 class MitarbeiterAdmin(admin.ModelAdmin):
-    list_display = ('Vorname','Nachname','email','id')
+    list_display = ('Vorname','Nachname','Anrede','email','Sex','id')
     inlines = [majournalInline]
 admin.site.register(Mitarbeiter,MitarbeiterAdmin)
 
 
 class JournalsAdmin(admin.ModelAdmin):
-    list_display = ('Name','id')
+    list_display = ('Name','Kurztitel','Quelle','id')
     inlines = [majournalInline]
     extra=1
 admin.site.register(Journals,JournalsAdmin)
