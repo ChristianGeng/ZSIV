263d76faa7bfd88b83ddcc579683fddab37f3fd5
ChristianGeng <christian.c.geng@gmail.com>
2018-05-10 14:57:56 +0200

    pep8 in urls.py

--- mysite_MYSQL/urls.py
+++ mysite_MYSQL/urls.py
@@ -8,7 +8,7 @@ Function views
     2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+v    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
@@ -20,7 +20,9 @@ from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
 
-""" redirect http://localhost:8000/ oder zsiv.pythonanywhere.com/""" 
+""" redirect http://localhost:8000/ oder zsiv.pythonanywhere.com/"""
+
+
 class RootRedirectView(RedirectView):
 
     permanent = False
@@ -30,25 +32,19 @@ class RootRedirectView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         self.url = '/ZSIV/'
         return super(RootRedirectView, self).get_redirect_url(*args, **kwargs)
-        
-
-
 
 
 urlpatterns = [
     url(r'^$', RootRedirectView.as_view()),
-    #url(r'^$', redirect_to, {'ZSIV': ''}),
-    
+    # url(r'^$', redirect_to, {'ZSIV': ''}),
     url(r'^ZSIV/', include('ZSIV.urls')),
     url(r'^admin/', admin.site.urls),
     # django-registration-redux old
-    #url(r'^accounts/', include('registration.backends.default.urls')),
+    # url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
 
 # aus try django 1.9 , fuer fileupload locations
-
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
